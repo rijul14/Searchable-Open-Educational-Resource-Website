09be478b3f7051492d232467888fefe761cefab4
LongHuy Nguyen <longhuyn@usc.edu>
2021-09-16 11:02:13 -0700

    Add navbar to home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/home/home";
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const history = useHistory();
+
+    return (
+        <div className="App">
+            <Router history={history}>
+                <Switch>
+                    <Route path="/login">
+                        {/* <Login/> */}
+                    </Route>
+                    <Route path="/home">
+                        <Home/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
