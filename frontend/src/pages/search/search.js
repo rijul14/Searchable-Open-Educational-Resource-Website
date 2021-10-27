dfae4ca524a01cd9d5881234ff3a68733abe6e3c
Sakura Rapolu <rapolu@usc.edu>
2021-10-27 16:17:54 -0700

    added initial search bar

--- frontend/src/pages/search/search.js
+++ frontend/src/pages/search/search.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./search.css";
 import Result from "../../components/result/result"
 import Grid from "@mui/material/Grid";
+import Category from "../../components/search/category";
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -23,7 +24,18 @@ export default class Home extends React.Component {
             <div className="h-100">
                 <Grid className="h-100" container spacing={2}>
                     <Grid item xs={3}>
-                        <div className="p-4 h-100">Search here</div>
+                        <div className="p-4 h-100">
+                        <div className="searchBar">
+                            <input placeholder={"Search keywords..."}></input>
+                            <button className="btn btn-primary">Search</button>
+                        </div>
+                            {/* form with a checklist */}
+                            <Category category="Tecnología" options={["Video", "Peardeck", "Google Form", "Quizlet"]}></Category>
+                            <Category category="Nivel" options={["Spanish Level 1- BÁSICO", "Spanish Level 2- INTERMEDIO", "Spanish Level 3- AVANZADO"]}></Category>
+                            <Category category="Destrezas" options={["Comprensión auditiva", "Comprensión de textos", "Comprensión de palabras", "Comprensión de frases", "Comprensión de oraciones"]}></Category>
+                            <Category category="Gramática" options={["Verbos", "Adjetivos", "Sustantivos", "Adverbios", "Preposiciones"]}></Category>
+                            <Category category="Vocabulario" options={["Materiales de clase", "Acciones habituales en un día de clase", "Acciones habituales en un día de clase", "Acciones habituales en un día de clase", "Acciones habituales en un día de clase"]}></Category>
+                        </div>
                     </Grid>
                     <Grid item xs={9}>
                         <div className="p-4 bg-light h-100">
