fe80a0ad0b14f14baf22e884721fab129655d955
LongHuy Nguyen <longhuyn@usc.edu>
2021-11-09 15:53:37 -0800

    Fix home page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./home.css";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div class="container-fluid">
-                <div class = "row body flex-nowrap">
-                    <div class="bg-image">
-                    </div>
+            <div class="container-fluid" style={{ position: "relative" }}>
+                <div class="row body flex-nowrap">
+                    <div class="bg-image"></div>
                     <div class="bg-text">
-                        <div class="title" >
-                        Oerinspanish <br/>
+                        <div class="title">
+                            Oerinspanish <br />
                         </div>
-                        Recursos educativos de acceso libre <br/>
-                        para la enseñanza del español <br/> <br/> <br/>
-                        <div class="search" >
-                            <Link style={{ color: 'inherit'}}
-                                  to="/search">búsqueda</Link><br/>
+                        Recursos educativos de acceso libre <br />
+                        para la enseñanza del español <br /> <br /> <br />
+                        <div class="search">
+                            <Link style={{ color: "inherit" }} to="/search">
+                                búsqueda
+                            </Link>
+                            <br />
                         </div>
                     </div>
                 </div>
